63e76150e9d8dc5771287365e99eeeecc55a6043
oktaysabak <oktaysabak@yandex.com>
2019-03-10 20:28:22 +0300

    order and branch visualization, we need classificate them

--- matplot/main.py
+++ matplot/main.py
@@ -1,14 +1,13 @@
 import csv
 from math import sqrt, pi, sin, cos, atan2
-RADIUS = 6371
-RED_MIN = 20
-RED_MAX = 30
-GREEN_MIN = 35
-GREEN_MAX = 50
-BLUE_MIN = 20
-BLUE_MAX = 80
-branchs = [[41.049792,29.003031],[41.069940,29.019250],[41.049997,29.026108]]
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+branchs_X = [41.049792,41.069940,41.049997]
+branchs_Y = [29.003031,29.019250,29.026108]
 orders = []
+orders_X = []
+orders_Y = []
 with open('orders.csv') as order_file:
     csv_reader = csv.reader(order_file, delimiter=',')
 
@@ -16,32 +15,10 @@ with open('orders.csv') as order_file:
         
         orders.append([float(x) for x in row])
 
-def degree2Radian(degree):
-    return degree*(pi/180)
-
-
-def getDistance(branch, order):
-    lat = degree2Radian(order[0]-branch[0])
-    lng = degree2Radian(order[1]-branch[0])
-
-    haversine_a = sin(lat/2)*sin(lat/2) + cos(degree2Radian(branch[0]))*cos(degree2Radian(order[0]))* sin(lng/2)* sin(lng/2)
-    haversine_c = 2* atan2(sqrt(haversine_a),sqrt(1-haversine_a))
-
-    return RADIUS*haversine_c
-red = []
-green = []
-blue = []
 for o in orders:
-    red.append(getDistance(branchs[0],o))
-    green.append(getDistance(branchs[1],o))
-    blue.append(getDistance(branchs[2],o))
+    orders_X.append(o[0])
+    orders_Y.append(o[1])
 
-red_orders = []
-green_orders = []
-blue_orders = []
-for i,v in enumerate(red):
-    print(str(i)+". red {} green {} blue {} ".format(*[v,green[i], blue[i]]))
-
-# print("redMax::",str(max(red)), "redMin::",str(min(red)))
-# print("greenMax::",str(max(green)), "greenMin::",str(min(green)) )
-# print("blueMax::",str(max(blue)), "blueMin::",str(min(blue)) )
+plt.scatter(branchs_X,branchs_Y)
+plt.scatter(orders_X, orders_Y,marker='X', c="g")
+plt.show()
