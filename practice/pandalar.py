936eb32896ae64cbb78a246c0faf03ef4abf30f9
oktaysabak <oktaysabak@yandex.com>
2019-03-24 02:07:24 +0300

    pandalar updated

--- practice/pandalar.py
+++ practice/pandalar.py
@@ -1,1 +1,10 @@
-import pandas
+import pandas as pd
+
+#dataframe için uygun bir kütüphane
+
+dictionary = {"name":["ali", "ahmet", "oktay"],"age":[13,14,15],"salary":[100,200,300]}
+
+dataframe1 = pd.DataFrame(dictionary)
+
+head = dataframe1.head(7)
+tail = dataframe1.tail(2)
