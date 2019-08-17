2d0e7addac943a58860f6348e5cb5bd7e6b94443
oktaysabak <oktaysabak@yandex.com>
2019-08-18 02:34:54 +0300

    admin page completed

--- mdn-django/catalog/admin.py
+++ mdn-django/catalog/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from catalog.models import (
+    Author,
+    Genre,
+    Book,
+    BookInstance
+)
 
-# Register your models here.
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BooksInline(admin.TabularInline):
+    model = Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+admin.site.register(Author, AuthorAdmin)
+
+admin.site.register(Genre)
